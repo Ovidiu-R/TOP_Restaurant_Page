b3231f943cf37b5836312b718eb1d531fcbcb493
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-04-05 09:36:43 +0100

    Add image imports

--- src/home.js
+++ src/home.js
@@ -1,18 +1,19 @@
 import './style.css';
+import homeImage from '../media/SakuraTrattoria.png'
 
 const loadHomePage = () => {
     const title = document.createElement('h1');
     const main = document.createElement('div');
-    const image = document.createElement('img');
+    const image = new Image();
     const headline = document.createElement('p');
     content.textContent = "";
     
 
     title.textContent = 'TRATTORIA SAKURA';
     main.setAttribute('id', 'main');
-    image.setAttribute('src', '../media/SakuraTrattoria.png');
-    image.setAttribute('alt', 'Photo of a delicious looking pizza topped with cuts of sashimi');
-    image.setAttribute('id', 'homeImage');
+    image.src = homeImage;
+    image.alt = 'Photo of a delicious looking pizza topped with cuts of sashimi';
+    image.id = 'homeImage';
     headline.setAttribute('id', 'headline');
     headline.innerHTML = 'Trattoria Sakura blends Italian and Japanese cuisines, offering a fusion of flavors in a stylish setting. Dishes like sushi-style bruschetta and miso-infused risotto showcase the creativity of the chefs, using fresh, locally-sourced ingredients. Experience a unique culinary journey that celebrates the best of both worlds at Sakura Trattoria. Explore our delicious menu today!';
     main.append(image, headline);
--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,16 @@
 import './style.css';
+import menu from '../media/menu.jpeg'
 
 const loadMenu = () => {
     content.textContent = "";
     content.style.marginTop = '0px';
     content.style.marginLeft = '0px';
     content.style.marginRight = '0px';
-    const menu = document.createElement('img');
-    menu.setAttribute('src', '../media/menu.jpeg');
-    menu.setAttribute('alt', 'Delicious-looking, totally not AI-generated menu');
-    menu.classList.add('menu');
-    content.append(menu);
+    const myMenu = new Image();
+    myMenu.src = menu;
+    myMenu.alt = 'Delicious-looking, totally not AI-generated menu';
+    myMenu.classList.add('menu');
+    content.append(myMenu);
 }
 
 export {loadMenu};
